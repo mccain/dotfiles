22d21f411463403a761b995c29344283dcef2705
Daniel S. McCain <dsmccain@acm.org>
2014-10-20 21:53:55 +0200

    Added z (github.com/rupa/z) to Zsh

--- bootstrap_scripts/bootstrap_zsh.sh
+++ bootstrap_scripts/bootstrap_zsh.sh
@@ -5,7 +5,16 @@ checkAndBackup $HOME/.zshrc
 ln -sf $PWD/zsh/zshrc $HOME/.zshrc
 
 mkdir -p $DOTFILES/extra
+# Create the file if it doesn't exist
+ZSH_EXTRA=$DOTFILES/extra/zsh_extra
+if [[ ! -e $ZSH_EXTRA ]]; then
+    touch $ZSH_EXTRA
+fi
 
 OMZ=$DOTFILES/extra/oh-my-zsh
 rm -rf $OMZ
 git clone https://github.com/robbyrussell/oh-my-zsh $OMZ
+
+Z=$DOTFILES/extra/z
+rm -rf $Z
+git clone https://github.com/rupa/z $Z
