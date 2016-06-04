5f70b0d7407128cd6615c2dbc046bfe62b2cd61c
Daniel S. McCain <dsmccain@acm.org>
2016-06-04 21:44:01 +0200

    Add tmux resurrect plugin

--- bootstrap_scripts/bootstrap_tmux.sh
+++ bootstrap_scripts/bootstrap_tmux.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+TMUX_EXTRA_FOLDER="$PWD/extra/tmux"
+
 backupAndLink $PWD/tmux/tmux.conf $HOME/.tmux.conf
+backupAndLink "$TMUX_EXTRA_FOLDER" "$HOME/.tmux"
+
+mkdir -p "$TMUX_EXTRA_FOLDER"
+mkdir -p "$TMUX_EXTRA_FOLDER/plugins"
+
+TMUX_PLUGINS="$TMUX_EXTRA_FOLDER/plugins/tpm"
+rm -rf "$TMUX_PLUGINS"
+git clone --depth 1 https://github.com/tmux-plugins/tpm "$TMUX_PLUGINS"
