f3806f5b49c23005fa5d78502e21297310eacddb
Daniel S. McCain <dsmccain@acm.org>
2025-04-13 13:05:31 -0400

    Add gruvbox colorschemes to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -938,13 +938,46 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+  -- Colorschemes
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
+    'sainnhe/gruvbox-material',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      vim.g.gruvbox_material_foreground = 'mix'
+      vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_ui_contrast = 'high'
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_disable_italic_comment = 0
+      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+
+  {
+    'ellisonleao/gruvbox.nvim',
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('gruvbox').setup {
+        terminal_colors = true,
+        styles = {
+          italic = {
+            comments = true,
+          },
+        },
+      }
+
+      -- vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+
+  {
+    'folke/tokyonight.nvim',
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
@@ -956,7 +989,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
